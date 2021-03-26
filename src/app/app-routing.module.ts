2768f780beea9176d0852049b9904b87655555e8
ManuelAndrade123 <64620322+ManuelAndrade123@users.noreply.github.com>
2021-03-26 12:54:28 -0500

    Login usuario
    Ingreso del usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    component:LoginComponent
+  },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: 'comunas',
     loadChildren: () => import('./pages/comunas/comunas.module').then(m => m.ComunasModule) },
   { path: 'formulariocomunas', loadChildren: () => import('./pages/formulariocomunas/formulariocomunas.module').then(m => m.FormulariocomunasModule) },
   { path: 'formulariobarrios', loadChildren: () => import('./pages/formulariobarrios/formulariobarrios.module').then(m => m.FormulariobarriosModule) },
   { path: 'barrios', loadChildren: () => import('./pages/barrios/barrios.module').then(m => m.BarriosModule) },
-  { path: 'habitantes', loadChildren: () => import('./pages/GestionHabitantes/habitantes/habitantes.module').then(m => m.HabitantesModule) }
+  { path: 'habitantes', loadChildren: () => import('./pages/GestionHabitantes/habitantes/habitantes.module').then(m => m.HabitantesModule) },
+  
 ];
 
 @NgModule({
