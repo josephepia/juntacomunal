d27284e9bcbe1429c4e9c2d4ca6a1b8ab9a1bb77
joseph epiayu fernandez <remixlebo@gmail.com>
2021-04-07 23:36:49 -0500

    actualizacion union de ramas
    conflictos corregidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import {MatStepperModule} from '@angular/material/stepper';
 import {CdkStepperModule} from '@angular/cdk/stepper';
