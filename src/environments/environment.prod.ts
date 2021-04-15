18c7f8b7a574c8973f1663720ffc26eb45f1a41f
joseph epiayu fernandez <remixlebo@gmail.com>
2021-04-15 00:00:41 -0500

    integracion con travis ci
    integracion con la herramienta de integracion continua.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAnrlKVK2AIwPz65GnDbYqISq6OBJF-0Jk",
+    authDomain: "junta-de-accion-comunal.firebaseapp.com",
+    projectId: "junta-de-accion-comunal",
+    storageBucket: "junta-de-accion-comunal.appspot.com",
+    messagingSenderId: "293850188300",
+    appId: "1:293850188300:web:1ead76696fe224f2943c29",
+    measurementId: "G-F9QYKEDRGB"
+  }
 };
